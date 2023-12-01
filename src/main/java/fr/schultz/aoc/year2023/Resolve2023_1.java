f980f0c2e949103e1009d38e9692f0410006780c
thomasschultz <thomas.schultz@lde.fr>
2023-12-01 18:27:10 +0100

    day 1 : code quality

--- src/main/java/fr/schultz/aoc/year2023/Resolve2023_1.java
+++ src/main/java/fr/schultz/aoc/year2023/Resolve2023_1.java
@@ -4,7 +4,7 @@ package fr.schultz.aoc.year2023;
 import fr.schultz.generator.AocTemplate;
 
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Resolve2023_1 extends AocTemplate {
 
@@ -31,24 +31,23 @@ public class Resolve2023_1 extends AocTemplate {
 
 
     public String resolvePart2(String input) {
-
         List<String> data = this.getData(input);
-        Integer res = data.stream().map(line -> {
-            line = line.replaceAll("one", "o1e");
-            line = line.replaceAll("two", "t2o");
-            line = line.replaceAll("three", "t3e");
-            line = line.replaceAll("four", "f4r");
-            line = line.replaceAll("five", "f5i");
-            line = line.replaceAll("six", "s6x");
-            line = line.replaceAll("seven", "s7n");
-            line = line.replaceAll("eight", "e8t");
-            line = line.replaceAll("nine", "n9e");
-            line = line.replaceAll("zero", "z0o");
-            String num = line.replaceAll("[a-z]", "");
-            num = num.charAt(0) + "" + num.charAt(num.length() - 1);
-            return Integer.parseInt(num);
-        }).reduce(0, Integer::sum);
-        return res.toString();
+        int res = data.parallelStream().mapToInt(line -> {
+                    String modifiedLine = line.replaceAll("one", "o1e")
+                            .replaceAll("two", "t2o")
+                            .replaceAll("three", "t3e")
+                            .replaceAll("four", "f4r")
+                            .replaceAll("five", "f5e")
+                            .replaceAll("six", "s6x")
+                            .replaceAll("seven", "s7n")
+                            .replaceAll("eight", "e8t")
+                            .replaceAll("nine", "n9e")
+                            .replaceAll("[a-z]", "");
+                    modifiedLine = modifiedLine.charAt(0) + "" + modifiedLine.charAt(modifiedLine.length() - 1);
+                    return Integer.parseInt(modifiedLine);
+                })
+                .sum();
+        return Integer.toString(res);
 
     }
 
